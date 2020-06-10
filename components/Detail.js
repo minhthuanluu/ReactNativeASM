a24996f4558774049ef61b569d7beb9febb3a753
trangvnkps08516 <55426902+trangvnkps08516@users.noreply.github.com>
2020-06-11 01:28:57 +0700

    a

--- components/Detail.js
+++ components/Detail.js
@@ -18,10 +18,7 @@ export default function Detail({ route, navigation }) {
         <View style={{ flex: 1 }}>
             <ScrollView showsVerticalScrollIndicator={false}>
             <Image source={{ uri: item.image }} style={{ justifyContent:'center',alignSelf:'center',width: 200, height: 70, borderRadius: 10 }} />
-
                 <Text style={styles.content}>{item.content}</Text>
-
-            
             <View style={styles.commentContent}>
                 <TouchableOpacity onPress={() => setliked(!liked)} style={{ position: 'absolute', margin: 10, right: 20, top: 410 }}>
                     <Image source={liked == true ? require('./images/liked.png') : require('./images/like.png')} resizeMode='cover' style={{ width: 30, height: 30 }} />
